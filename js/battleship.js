2be5798cf4532ae1a834d1138cff518c3edc90d3
Adrian Smuzniak <adriansmuzniak@gmail.com>
2017-07-25 19:22:57 +0200

    task updates/battleship code

--- js/battleship.js
+++ js/battleship.js
@@ -7,17 +7,32 @@ var location3 = 5;
 
 // próby zatopienia okrętu
 var guess; //wskazanie do sprawdzenia
-var guessess = 0; //liczba prób
+var guesses = 0; //liczba prób
 var hits = 0; //trafione
 
 //wartość - czy okręt został zatopiony?
 var isSunk = false;
 
-while (isSunk == true) {
-      guess = prompt("Gotów? Cel, pal! (podaj liczbę z zakresu 0-6):"
+  while (isSunk == false) {
+      guess = prompt("Gotów? Cel, pal! (podaj liczbę z zakresu 0-6):");
       if (guess < 0 || guess > 6) {
           alert("Proszę podać prawidłową liczbę z zakresu od 0-6!");
       } else {
-          guessess = guessess + 1;
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+          hits = hits + 1;
+          alert("TRAFIONY!!");
+          if (hits == 3) {
+            isSunk == true;
+            alert("Hej, zatopiłeś mój okręt!");9
+          }
+        } else if (guess == null || guess == false) {
+          alert("Ej, podaj jakąś liczbę")
+        } else {
+          alert("PUDŁO!")
+        }
+      }
   }
-}
+  var stats = "Potrzebowałeś " + guesses + " ruchów, aby zatopić okręt. Twoja efektywność strzelecka wynosi: " + (3/guesses) + ".";
+  alert(stats);
