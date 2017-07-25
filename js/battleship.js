6c3d79787c10ebeb122a741617455d6504ffc1bc
Adrian Smuzniak <adriansmuzniak@gmail.com>
2017-07-25 16:49:25 +0200

    task updates/battleship adds

---
+++ js/battleship.js
@@ -0,0 +1,14 @@
+//Gra w okręty - początek
+
+// położenie okrętu
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+// próby zatopienia okrętu
+var guess; //wskazanie do sprawdzenia
+var guessess = 0; //odgadnięte
+var hits = 0; //trafione
+
+//wartość - czy okręt został zatopiony?
+var isSink = false;
