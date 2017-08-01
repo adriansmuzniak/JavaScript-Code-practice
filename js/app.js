c3f68b6bb59d7b36dc1eb1d3629a37c169aad174
Adrian Smuzniak <adriansmuzniak@gmail.com>
2017-08-01 11:33:44 +0200

    task updates

--- js/app.js
+++ js/app.js
@@ -513,3 +513,24 @@ console.log(flavors.length);
 var iceCreamLength = flavors.length;
 console.log(iceCreamLength);
 // -----------------------------------
+
+function makePhrases() {
+  var words1 = ["nieprzerwane", "wielowarstwowe", "tysiącmetrowe", "biznesowe", "niepowstrzymane"];
+  var words2 = ["wspomagane", "wartościowe", "zorientowane", "skoncentrowane", "wyrównane"];
+  var words3 = ["procesy", "rozwiązania", "punkty", "strategie", "wizje"];
+
+  var rand1 = Math.floor(Math.random() * words1.length);
+  var rand2 = Math.floor(Math.random() * words2.length);
+  var rand3 = Math.floor(Math.random() * words3.length);
+
+  var phrase = words1[rand1] + " " + words2[rand2] + " " + words3[rand3];
+  console.log(phrase);
+}
+
+makePhrases();
+makePhrases();
+makePhrases();
+makePhrases();
+makePhrases();
+
+//--------------------------------------
