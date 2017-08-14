e64dace32a4ba0956900128f6e6515e88ded19b5
Adrian Smuzniak <adriansmuzniak@gmail.com>
2017-08-14 16:48:52 +0200

    task updates

--- js/app.js
+++ js/app.js
@@ -1048,3 +1048,25 @@ console.log("code2");
 //-------------------------------------------
 
 //DOCUMENT OBJECT MODEL
+
+function init() {
+  var planet = document.getElementById("greenplanet");
+  planet.innerHTML = "Alarm czerwony: rozpoczęto ostrzał z fazerów";
+  console.log(planet.innerHTML);
+  }
+
+window.onload = init;
+
+//-----------------------------------------
+
+function addSongs() {
+  var song1 = document.getElementById("song1");
+  var song2 = document.getElementById("song2");
+  var song3 = document.getElementById("song3");
+
+  song1.innerHTML = "Smutne zamszowe łańcuchy, Elvisa Pagelya";
+  song2.innerHTML = "Wielkie obiekty w ogniu, Jerry'ego JSON Lewisa";
+  song3.innerHTML = "Pierwszy wiersz kodu, Johnny'ego JavaScripta";
+}
+
+window.onload = addSongs;
