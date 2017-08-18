60942cbd76f298ae95988485e66590ef52d32e80
Adrian Smuzniak <adriansmuzniak@gmail.com>
2017-08-18 19:30:55 +0200

    task updates

--- js/app.js
+++ js/app.js
@@ -1484,3 +1484,14 @@ function validate2(phoneNumber) {
   return true;
 }
 //-------------------------------------
+
+function Duck(sound) {
+  this.sound = sound;
+  this.quack = function() {console.log(this.sound);}
+}
+
+var toy = new Duck("kwa kwa");
+toy.quack();
+
+console.log(typeof toy);
+console.log(toy instanceof Duck);
