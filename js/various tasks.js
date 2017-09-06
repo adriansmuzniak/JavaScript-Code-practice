4223f12ce00525257da32063e20edb4bb02e4bc9
Adrian Smuzniak <adriansmuzniak@gmail.com>
2017-09-06 19:28:23 +0200

    various task from codeacademy

--- js/various tasks.js
+++ js/various tasks.js
@@ -25,3 +25,52 @@ if (moonPhase === "full" || isFoggyNight == true) {
 } else {
   console.log("invalid moon phase");
 }
+
+//------------------
+
+// let moonPhase = "full";
+
+switch (moonPhase) {
+  case "full":
+    console.log("Howl!");
+    break;
+  case "mostly full":
+    console.log("Arms and legs are getting hairier");
+    break;
+  case "mostly new":
+    console.log("Back on two feet");
+    break;
+  default:
+    console.log("Invalid moon phase");
+    break;
+ }
+
+ //--------------------
+
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+
+//-------------------
+
+const takeOrder = () => {
+  console.log("Order: pizza!");
+}
+
+takeOrder();
+
+const multiplyYouFool = (x, y, z) => {
+  let damn = ((x * y) * 1000) / 2000003;
+  return damn;
+}
+
+const sth = multiplyYouFool(30, 20, 10);
+console.log(sth);
