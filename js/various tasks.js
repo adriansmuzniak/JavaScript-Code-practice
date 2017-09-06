c41d5698d3252d2a4f70807e88a4b234fb591eb9
Adrian Smuzniak <adriansmuzniak@gmail.com>
2017-09-06 18:24:56 +0200

    task

--- js/various tasks.js
+++ js/various tasks.js
@@ -12,3 +12,16 @@ console.log(i);
 // answer: false;
 
 //-------------------
+
+let moonPhase = "full";
+let isFoggyNight = false;
+
+if (moonPhase === "full" || isFoggyNight == true) {
+  console.log("Howl");
+} else if (moonPhase === "mostly full") {
+  console.log("Arms and legs are getting hairier");
+} else if (moonPhase === "mostly new") {
+  console.log("Back on two feet");
+} else {
+  console.log("invalid moon phase");
+}
