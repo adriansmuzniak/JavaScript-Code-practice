9c43acc25b8bfd61717757002a7ebcd95a76eaff
Adrian Smuzniak <adriansmuzniak@gmail.com>
2017-09-14 09:33:18 +0200

    cd tasks

--- js/various tasks.js
+++ js/various tasks.js
@@ -126,3 +126,15 @@ myNightSky();
 console.log(stars);
 
 //-------------------
+
+const visibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+  let region = 'The Arctic';
+    if (region === 'The Arctic') {
+      let lightWaves = 'Northern Lights';
+      console.log(lightWaves);
+    }
+  console.log(lightWaves);
+};
+
+visibleLightWaves();
